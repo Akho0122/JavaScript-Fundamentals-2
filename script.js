1476b774a72aacbd0cd4295dcb3e74e5ccc46a88
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-03 18:21:55 +0500

    functions inside the function

--- script.js
+++ script.js
@@ -30,30 +30,43 @@
 
 
 // FUNCTION DECLARATION
-function calcAge(birthYear){
-    return 2022 - birthYear;
+// function calcAge(birthYear){
+//     return 2022 - birthYear;
+// }
+// const age1 = calcAge(2002);
+
+// // FUNCTION EXPRESSION
+// const calcAge2 = function(birthYear){
+//     return 2022 - birthYear;
+// }
+// const age2 = calcAge2(2002);
+
+// console.log(age1, age2);
+
+// // ARROW FUNCTION 
+// const calcAge3 = (birthYear) => 2037 -birthYear;
+// const age3 = calcAge3(2002);
+// console.log(age3);
+
+// const yearUntilRetirement = (birthYear, firstName) =>{
+//     const age = 2022 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+
+// }
+// console.log(yearUntilRetirement(2002, "John"));
+// console.log(yearUntilRetirement(1999, "Bob"));
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
 }
-const age1 = calcAge(2002);
 
-// FUNCTION EXPRESSION
-const calcAge2 = function(birthYear){
-    return 2022 - birthYear;
-}
-const age2 = calcAge2(2002);
-
-console.log(age1, age2);
-
-// ARROW FUNCTION 
-const calcAge3 = (birthYear) => 2037 -birthYear;
-const age3 = calcAge3(2002);
-console.log(age3);
-
-const yearUntilRetirement = (birthYear, firstName) =>{
-    const age = 2022 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`;
-
-}
-console.log(yearUntilRetirement(2002, "Akho"));
-console.log(yearUntilRetirement(1999, "Bob"));
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} 
+    apples and ${orangePieces} oranges.`;
+    return juice;
+};
+console.log(fruitProcessor(2, 3));
