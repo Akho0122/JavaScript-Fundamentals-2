6a999cc76d524f19baa726fb56262b4a347631e1
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-04 14:21:29 +0500

    While loop

--- script.js
+++ script.js
@@ -318,21 +318,41 @@
 //     console.log(jonasArray[i], typeof jonasArray[i]);
 // }
 
-const jonas = [
-    'Jonas', 
-    'Walker',
-    2022 - 2002, 
-    'teacher',
-    ['Michael', 'Peter', 'Steven']
-];
+// const jonas = [
+//     'Jonas', 
+//     'Walker',
+//     2022 - 2002, 
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ];
 
-for(let i = jonas.length - 1; i >= 0; i--){
-    console.log(jonas[i]);
+// for(let i = jonas.length - 1; i >= 0; i--){
+//     console.log(jonas[i]);
+// }
+
+// for(let exercise = 1; exercise < 4; exercise++){
+//     console.log(`Starting exercise ${exercise}`);
+//     for(let rep = 1; rep < 6; rep++){
+//         console.log(`Exercise${exercise} Lifting weight repetition ${rep}`)
+//     }
+// }
+
+// for(let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+let rep = 1;
+while(rep <= 10){
+    // console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
 }
 
-for(let exercise = 1; exercise < 4; exercise++){
-    console.log(`Starting exercise ${exercise}`);
-    for(let rep = 1; rep < 6; rep++){
-        console.log(`Exercise${exercise} Lifting weight repetition ${rep}`)
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while(dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6){
+        console.log(`Loop is about to end...`)
     }
 }
